2dd9b57c5852617fe47fb429dec477740da7865b
Pavel Alekseev <pavelekseev@gmail.com>
2020-04-06 22:50:14 +0300

    Add success field to woney state

--- src/core/state/woney/reducer.js
+++ src/core/state/woney/reducer.js
@@ -15,17 +15,20 @@ const reducer = handleActions(
     }),
     [WONEY_REQUEST_REQUEST]: (state, action) => ({
       ...state,
+      success: false,
       error: null,
     }),
     [WONEY_REQUEST_SUCCESS]: (state) => ({
       ...state,
+      success: true,
     }),
     [WONEY_REQUEST_FAILURE]: (state, action) => ({
       ...state,
+      success: false,
       error: action.payload,
     }),
   },
-  { data: null, error: null },
+  { data: null, success: false, error: null },
 );
 
 export default reducer;
--- src/core/state/woney/selectors.js
+++ src/core/state/woney/selectors.js
@@ -1,3 +1,5 @@
 export const getData = ({ woney }) => woney.data;
 
+export const getSuccess = ({ woney }) => woney.success;
+
 export const getWoneyError = ({ woney }) => woney.error;
