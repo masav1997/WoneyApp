32e3d58ff16949aa15d99c1f9486e6ceb4798ea6
Pavel Alekseev <pavelekseev@gmail.com>
2020-04-06 23:40:00 +0300

    Send form to request

--- src/core/api/woney.js
+++ src/core/api/woney.js
@@ -4,5 +4,7 @@ const axiosInstance = axios.create({
   baseURL: 'https://admin.woney.com'
 });
 
-export const request = ({ surname, email, eth_wallet, ticket }) =>
-  axiosInstance.post('/api/request', { surname, email, eth_wallet, ticket});
+export const request = (form) =>
+  axiosInstance.post('/api/request', form, { headers: {
+    'Content-Type': 'multipart/form-data;'
+  }});
