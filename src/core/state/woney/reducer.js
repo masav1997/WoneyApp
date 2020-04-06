b0ebaefa8c6680674d84c90276bca53c74b23f41
Pavel Alekseev <pavelekseev@gmail.com>
2020-04-06 21:38:16 +0300

    Add reducer to make request

---
+++ src/core/state/woney/reducer.js
@@ -0,0 +1,31 @@
+import { handleActions } from 'redux-actions';
+
+import {
+  WONEY_UPDATE_FORM_DATA,
+  WONEY_REQUEST_REQUEST,
+  WONEY_REQUEST_SUCCESS,
+  WONEY_REQUEST_FAILURE,
+} from './actions';
+
+const reducer = handleActions(
+  {
+    [WONEY_UPDATE_FORM_DATA]: (state, action) => ({
+      ...state,
+      data: action.payload,
+    }),
+    [WONEY_REQUEST_REQUEST]: (state, action) => ({
+      ...state,
+      error: null,
+    }),
+    [WONEY_REQUEST_SUCCESS]: (state) => ({
+      ...state,
+    }),
+    [WONEY_REQUEST_FAILURE]: (state, action) => ({
+      ...state,
+      error: action.payload,
+    }),
+  },
+  { data: null, error: null },
+);
+
+export default reducer;
